1249d5cc599a42f85a7d415a2be38f8d98147207
Sven <fr.sven.fr@gmail.com>
2025-02-09 13:06:52 +0100

    close modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { berachain } from '@reown/appkit/networks'
 import { useAppKitAccount, useAppKitNetwork } from '@reown/appkit/react'
 import { useEffect } from 'react'
+import { modal } from './context'
 
 export default function Home() {
   const { switchNetwork, caipNetwork } = useAppKitNetwork()
@@ -14,6 +15,7 @@ export default function Home() {
     if (status === 'connected') {
       if (caipNetwork?.id !== URL_CHAIN.id) {
         switchNetwork(URL_CHAIN)
+        modal?.close()
       }
     }
   }, [status])
